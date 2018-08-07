yamamoto-febc/terraform-provider-arukas-1

Update dependencies

vendor/github.com/yamamoto-febc/go-arukas/service.go
@@ -1,0 +1,216 @@
+package arukas
+
+import "time"
+
+// ServiceListData represents services data
+type ServiceListData struct {
+	Data []*Service `json:"data"`
+}
+
+// Service represents service object
+type Service struct {
+	ID            string               `json:"id"`
+	TempID        int32                `json:"temp_id,omitempty"`
+	Type          string               `json:"type,omitempty"`
+	Attributes    *ServiceAttr         `json:"attributes,omitempty"`
+	Relationships *ServiceRelationship `json:"relationships,omitempty"`
+}
+
+// AppID returns data.attributes.app_id
+func (s *Service) AppID() string {
+	return s.Attributes.AppID
+}
+
+// Image returns data.attributes.image
+func (s *Service) Image() string {
+	return s.Attributes.Image
+}
+
+// Command returns data.attributes.command
+func (s *Service) Command() string {
+	return s.Attributes.Command
+}
+
+// Instances returns data.attributes.instances
+func (s *Service) Instances() int32 {
+	return s.Attributes.Instances
+}
+
+// CPUs returns data.attributes.cups
+func (s *Service) CPUs() float32 {
+	return s.Attributes.CPUs
+}
+
+// Memory returns data.attributes.memory
+func (s *Service) Memory() int32 {
+	return s.Attributes.Memory
+}
+
+// Environment returns data.attributes.environment
+func (s *Service) Environment() []*Env {
+	return s.Attributes.Environment
+}
+
+// Ports returns data.attributes.ports
+func (s *Service) Ports() []*Port {
+	return s.Attributes.Ports
+}
+
+// PortMappings returns data.attributes.port_mappings
+func (s *Service) PortMappings() [][]*PortMapping {
+	return s.Attributes.PortMappings
+}
+
+// PortMapping returns data.attributes.port_mappings[0]
+func (s *Service) PortMapping() []*PortMapping {
+	if len(s.Attributes.PortMappings) == 0 {
+		return nil
+	}
+	return s.Attributes.PortMappings[0]
+}
+
+// CreatedAt returns data.attributes.created_at
+func (s *Service) CreatedAt() *time.Time {
+	return s.Attributes.CreatedAt
+}
+
+// UpdatedAt returns data.attributes.updated_at
+func (s *Service) UpdatedAt() *time.Time {
+	return s.Attributes.UpdatedAt
+}
+
+// Status returns data.attributes.status
+func (s *Service) Status() string {
+	return s.Attributes.Status
+}
+
+// SubDomain returns data.attributes.subdomain
+func (s *Service) SubDomain() string {
+	return s.Attributes.SubDomain
+}
+
+// EndPoint returns data.attributes.endpoint
+func (s *Service) EndPoint() string {
+	return s.Attributes.EndPoint
+}
+
+// PlanID returns data.relationship.service_plan.data.id
+func (s *Service) PlanID() string {
+	return s.Relationships.ServicePlan.Data.ID
+}
+
+// ServiceAttr represents service.attributes object
+type ServiceAttr struct {
+	AppID                    string           `json:"app_id,omitempty"`
+	Image                    string           `json:"image"`
+	Command                  string           `json:"command"`
+	Instances                int32            `json:"instances"`
+	CPUs                     float32          `json:"cups,omitempty"`
+	Memory                   int32            `json:"memory,omitempty"`
+	Environment              []*Env           `json:"environment"`
+	Ports                    []*Port          `json:"ports,omitempty"`
+	PortMappings             [][]*PortMapping `json:"port_mappings,omitempty"`
+	CreatedAt                *time.Time       `json:"created_at,omitempty"`
+	UpdatedAt                *time.Time       `json:"updated_at,omitempty"`
+	Status                   string           `json:"status,omitempty"`
+	SubDomain                string           `json:"subdomain,omitempty"`
+	EndPoint                 string           `json:"endpoint,omitempty"`
+	CustomDomains            []*CustomDomain  `json:"custom_domains,omitempty"`
+	LastInstanceFailedAt     *time.Time       `json:"last_instance_failed_at,omitempty"`
+	LastInstanceFailedStatus string           `json:"last_instance_failed_status,omitempty"`
+}
+
+// ServiceData represents service data
+type ServiceData struct {
+	Data *Service `json:"data"`
+}
+
+// ServiceID returns data.id
+func (s *ServiceData) ServiceID() string {
+	return s.Data.ID
+}
+
+// AppID returns data.attributes.app_id
+func (s *ServiceData) AppID() string {
+	return s.Data.Attributes.AppID
+}
+
+// Type returns data.type
+func (s *ServiceData) Type() string {
+	return s.Data.Type
+}
+
+// Image returns data.attributes.image
+func (s *ServiceData) Image() string {
+	return s.Data.Image()
+}
+
+// Command returns data.attributes.command
+func (s *ServiceData) Command() string {
+	return s.Data.Command()
+}
+
+// Instances returns data.attributes.instances
+func (s *ServiceData) Instances() int32 {
+	return s.Data.Instances()
+}
+
+// CPUs returns data.attributes.cups
+func (s *ServiceData) CPUs() float32 {
+	return s.Data.CPUs()
+}
+
+// Memory returns data.attributes.memory
+func (s *ServiceData) Memory() int32 {
+	return s.Data.Memory()
+}
+
+// Environment returns data.attributes.environment
+func (s *ServiceData) Environment() []*Env {
+	return s.Data.Environment()
+}
+
+// Ports returns data.attributes.ports
+func (s *ServiceData) Ports() []*Port {
+	return s.Data.Ports()
+}
+
+// PortMappings returns data.attributes.port_mappings
+func (s *ServiceData) PortMappings() [][]*PortMapping {
+	return s.Data.PortMappings()
+}
+
+// PortMapping returns data.attributes.port_mappings[0]
+func (s *ServiceData) PortMapping() []*PortMapping {
+	return s.Data.PortMapping()
+}
+
+// CreatedAt returns data.attributes.created_at
+func (s *ServiceData) CreatedAt() *time.Time {
+	return s.Data.CreatedAt()
+}
+
+// UpdatedAt returns data.attributes.updated_at
+func (s *ServiceData) UpdatedAt() *time.Time {
+	return s.Data.UpdatedAt()
+}
+
+// Status returns data.attributes.status
+func (s *ServiceData) Status() string {
+	return s.Data.Status()
+}
+
+// SubDomain returns data.attributes.subdomain
+func (s *ServiceData) SubDomain() string {
+	return s.Data.SubDomain()
+}
+
+// EndPoint returns data.attributes.endpoint
+func (s *ServiceData) EndPoint() string {
+	return s.Data.EndPoint()
+}
+
+// PlanID returns data.relationship.service_plan.data.id
+func (s *ServiceData) PlanID() string {
+	return s.Data.PlanID()
+}

vendor/github.com/zclconf/go-cty/cty/function/stdlib/format_fsm.go
@@ -1,0 +1,358 @@
+// line 1 "format_fsm.rl"
+// This file is generated from format_fsm.rl. DO NOT EDIT.
+
+// line 5 "format_fsm.rl"
+
+package stdlib
+
+import (
+	"bytes"
+	"fmt"
+	"unicode/utf8"
+
+	"github.com/zclconf/go-cty/cty"
+)
+
+// line 20 "format_fsm.go"
+var _formatfsm_actions []byte = []byte{
+	0, 1, 0, 1, 1, 1, 2, 1, 4,
+	1, 5, 1, 6, 1, 7, 1, 8,
+	1, 9, 1, 10, 1, 11, 1, 14,
+	1, 16, 1, 17, 1, 18, 2, 3,
+	4, 2, 12, 10, 2, 12, 16, 2,
+	12, 18, 2, 13, 14, 2, 15, 10,
+	2, 15, 18,
+}
+
+var _formatfsm_key_offsets []byte = []byte{
+	0, 0, 14, 27, 34, 36, 39, 43,
+	51,
+}
+
+var _formatfsm_trans_keys []byte = []byte{
+	32, 35, 37, 43, 45, 46, 48, 91,
+	49, 57, 65, 90, 97, 122, 32, 35,
+	43, 45, 46, 48, 91, 49, 57, 65,
+	90, 97, 122, 91, 48, 57, 65, 90,
+	97, 122, 49, 57, 93, 48, 57, 65,
+	90, 97, 122, 46, 91, 48, 57, 65,
+	90, 97, 122, 37,
+}
+
+var _formatfsm_single_lengths []byte = []byte{
+	0, 8, 7, 1, 0, 1, 0, 2,
+	1,
+}
+
+var _formatfsm_range_lengths []byte = []byte{
+	0, 3, 3, 3, 1, 1, 2, 3,
+	0,
+}
+
+var _formatfsm_index_offsets []byte = []byte{
+	0, 0, 12, 23, 28, 30, 33, 36,
+	42,
+}
+
+var _formatfsm_indicies []byte = []byte{
+	1, 2, 3, 4, 5, 6, 7, 10,
+	8, 9, 9, 0, 1, 2, 4, 5,
+	6, 7, 10, 8, 9, 9, 0, 13,
+	11, 12, 12, 0, 14, 0, 15, 14,
+	0, 9, 9, 0, 16, 19, 17, 18,
+	18, 0, 20, 3,
+}
+
+var _formatfsm_trans_targs []byte = []byte{
+	0, 2, 2, 8, 2, 2, 3, 2,
+	7, 8, 4, 3, 8, 4, 5, 6,
+	3, 7, 8, 4, 1,
+}
+
+var _formatfsm_trans_actions []byte = []byte{
+	7, 17, 9, 3, 15, 13, 25, 11,
+	43, 29, 19, 27, 49, 46, 21, 0,
+	37, 23, 40, 34, 1,
+}
+
+var _formatfsm_eof_actions []byte = []byte{
+	0, 31, 31, 31, 31, 31, 31, 31,
+	5,
+}
+
+const formatfsm_start int = 8
+const formatfsm_first_final int = 8
+const formatfsm_error int = 0
+
+const formatfsm_en_main int = 8
+
+// line 19 "format_fsm.rl"
+
+func formatFSM(format string, a []cty.Value) (string, error) {
+	var buf bytes.Buffer
+	data := format
+	nextArg := 1 // arg numbers are 1-based
+	var verb formatVerb
+
+	// line 153 "format_fsm.rl"
+
+	// Ragel state
+	p := 0          // "Pointer" into data
+	pe := len(data) // End-of-data "pointer"
+	cs := 0         // current state (will be initialized by ragel-generated code)
+	ts := 0
+	te := 0
+	eof := pe
+
+	// Keep Go compiler happy even if generated code doesn't use these
+	_ = ts
+	_ = te
+	_ = eof
+
+	// line 121 "format_fsm.go"
+	{
+		cs = formatfsm_start
+	}
+
+	// line 126 "format_fsm.go"
+	{
+		var _klen int
+		var _trans int
+		var _acts int
+		var _nacts uint
+		var _keys int
+		if p == pe {
+			goto _test_eof
+		}
+		if cs == 0 {
+			goto _out
+		}
+	_resume:
+		_keys = int(_formatfsm_key_offsets[cs])
+		_trans = int(_formatfsm_index_offsets[cs])
+
+		_klen = int(_formatfsm_single_lengths[cs])
+		if _klen > 0 {
+			_lower := int(_keys)
+			var _mid int
+			_upper := int(_keys + _klen - 1)
+			for {
+				if _upper < _lower {
+					break
+				}
+
+				_mid = _lower + ((_upper - _lower) >> 1)
+				switch {
+				case data[p] < _formatfsm_trans_keys[_mid]:
+					_upper = _mid - 1
+				case data[p] > _formatfsm_trans_keys[_mid]:
+					_lower = _mid + 1
+				default:
+					_trans += int(_mid - int(_keys))
+					goto _match
+				}
+			}
+			_keys += _klen
+			_trans += _klen
+		}
+
+		_klen = int(_formatfsm_range_lengths[cs])
+		if _klen > 0 {
+			_lower := int(_keys)
+			var _mid int
+			_upper := int(_keys + (_klen << 1) - 2)
+			for {
+				if _upper < _lower {
+					break
+				}
+
+				_mid = _lower + (((_upper - _lower) >> 1) & ^1)
+				switch {
+				case data[p] < _formatfsm_trans_keys[_mid]:
+					_upper = _mid - 2
+				case data[p] > _formatfsm_trans_keys[_mid+1]:
+					_lower = _mid + 2
+				default:
+					_trans += int((_mid - int(_keys)) >> 1)
+					goto _match
+				}
+			}
+			_trans += _klen
+		}
+
+	_match:
+		_trans = int(_formatfsm_indicies[_trans])
+		cs = int(_formatfsm_trans_targs[_trans])
+
+		if _formatfsm_trans_actions[_trans] == 0 {
+			goto _again
+		}
+
+		_acts = int(_formatfsm_trans_actions[_trans])
+		_nacts = uint(_formatfsm_actions[_acts])
+		_acts++
+		for ; _nacts > 0; _nacts-- {
+			_acts++
+			switch _formatfsm_actions[_acts-1] {
+			case 0:
+				// line 29 "format_fsm.rl"
+
+				verb = formatVerb{
+					ArgNum: nextArg,
+					Prec:   -1,
+					Width:  -1,
+				}
+				ts = p
+
+			case 1:
+				// line 38 "format_fsm.rl"
+
+				buf.WriteByte(data[p])
+
+			case 4:
+				// line 49 "format_fsm.rl"
+
+				// We'll try to slurp a whole UTF-8 sequence here, to give the user
+				// better feedback.
+				r, _ := utf8.DecodeRuneInString(data[p:])
+				return buf.String(), fmt.Errorf("unrecognized format character %q at offset %d", r, p)
+
+			case 5:
+				// line 56 "format_fsm.rl"
+
+				verb.Sharp = true
+
+			case 6:
+				// line 59 "format_fsm.rl"
+
+				verb.Zero = true
+
+			case 7:
+				// line 62 "format_fsm.rl"
+
+				verb.Minus = true
+
+			case 8:
+				// line 65 "format_fsm.rl"
+
+				verb.Plus = true
+
+			case 9:
+				// line 68 "format_fsm.rl"
+
+				verb.Space = true
+
+			case 10:
+				// line 72 "format_fsm.rl"
+
+				verb.ArgNum = 0
+
+			case 11:
+				// line 75 "format_fsm.rl"
+
+				verb.ArgNum = (10 * verb.ArgNum) + (int(data[p]) - '0')
+
+			case 12:
+				// line 79 "format_fsm.rl"
+
+				verb.HasWidth = true
+
+			case 13:
+				// line 82 "format_fsm.rl"
+
+				verb.Width = 0
+
+			case 14:
+				// line 85 "format_fsm.rl"
+
+				verb.Width = (10 * verb.Width) + (int(data[p]) - '0')
+
+			case 15:
+				// line 89 "format_fsm.rl"
+
+				verb.HasPrec = true
+
+			case 16:
+				// line 92 "format_fsm.rl"
+
+				verb.Prec = 0
+
+			case 17:
+				// line 95 "format_fsm.rl"
+
+				verb.Prec = (10 * verb.Prec) + (int(data[p]) - '0')
+
+			case 18:
+				// line 99 "format_fsm.rl"
+
+				verb.Mode = rune(data[p])
+				te = p + 1
+				verb.Raw = data[ts:te]
+				verb.Offset = ts
+
+				err := formatAppend(&verb, &buf, a)
+				if err != nil {
+					return buf.String(), err
+				}
+				nextArg = verb.ArgNum + 1
+
+				// line 324 "format_fsm.go"
+			}
+		}
+
+	_again:
+		if cs == 0 {
+			goto _out
+		}
+		p++
+		if p != pe {
+			goto _resume
+		}
+	_test_eof:
+		{
+		}
+		if p == eof {
+			__acts := _formatfsm_eof_actions[cs]
+			__nacts := uint(_formatfsm_actions[__acts])
+			__acts++
+			for ; __nacts > 0; __nacts-- {
+				__acts++
+				switch _formatfsm_actions[__acts-1] {
+				case 2:
+					// line 42 "format_fsm.rl"
+
+				case 3:
+					// line 45 "format_fsm.rl"
+
+					return buf.String(), fmt.Errorf("invalid format string starting at offset %d", p)
+
+				case 4:
+					// line 49 "format_fsm.rl"
+
+					// We'll try to slurp a whole UTF-8 sequence here, to give the user
+					// better feedback.
+					r, _ := utf8.DecodeRuneInString(data[p:])
+					return buf.String(), fmt.Errorf("unrecognized format character %q at offset %d", r, p)
+
+					// line 363 "format_fsm.go"
+				}
+			}
+		}
+
+	_out:
+		{
+		}
+	}
+
+	// line 171 "format_fsm.rl"
+
+	// If we fall out here without being in a final state then we've
+	// encountered something that the scanner can't match, which should
+	// be impossible (the scanner matches all bytes _somehow_) but we'll
+	// flag it anyway rather than just losing data from the end.
+	if cs < formatfsm_first_final {
+		return buf.String(), fmt.Errorf("extraneous characters beginning at offset %i", p)
+	}
+
+	return buf.String(), nil
+}

hashicorp/terraform-provider-arukas

vendor/github.com/yamamoto-febc/go-arukas/service.go
@@ -1,0 +1,216 @@
+package arukas
+
+import "time"
+
+// ServiceListData represents services data
+type ServiceListData struct {
+	Data []*Service `json:"data"`
+}
+
+// Service represents service object
+type Service struct {
+	ID            string               `json:"id"`
+	LinkID        int32                `json:"lid,omitempty"`
+	Type          string               `json:"type,omitempty"`
+	Attributes    *ServiceAttr         `json:"attributes,omitempty"`
+	Relationships *ServiceRelationship `json:"relationships,omitempty"`
+}
+
+// AppID returns data.attributes.app_id
+func (s *Service) AppID() string {
+	return s.Attributes.AppID
+}
+
+// Image returns data.attributes.image
+func (s *Service) Image() string {
+	return s.Attributes.Image
+}
+
+// Command returns data.attributes.command
+func (s *Service) Command() string {
+	return s.Attributes.Command
+}
+
+// Instances returns data.attributes.instances
+func (s *Service) Instances() int32 {
+	return s.Attributes.Instances
+}
+
+// CPUs returns data.attributes.cups
+func (s *Service) CPUs() float32 {
+	return s.Attributes.CPUs
+}
+
+// Memory returns data.attributes.memory
+func (s *Service) Memory() int32 {
+	return s.Attributes.Memory
+}
+
+// Environment returns data.attributes.environment
+func (s *Service) Environment() []*Env {
+	return s.Attributes.Environment
+}
+
+// Ports returns data.attributes.ports
+func (s *Service) Ports() []*Port {
+	return s.Attributes.Ports
+}
+
+// PortMappings returns data.attributes.port_mappings
+func (s *Service) PortMappings() [][]*PortMapping {
+	return s.Attributes.PortMappings
+}
+
+// PortMapping returns data.attributes.port_mappings[0]
+func (s *Service) PortMapping() []*PortMapping {
+	if len(s.Attributes.PortMappings) == 0 {
+		return nil
+	}
+	return s.Attributes.PortMappings[0]
+}
+
+// CreatedAt returns data.attributes.created_at
+func (s *Service) CreatedAt() *time.Time {
+	return s.Attributes.CreatedAt
+}
+
+// UpdatedAt returns data.attributes.updated_at
+func (s *Service) UpdatedAt() *time.Time {
+	return s.Attributes.UpdatedAt
+}
+
+// Status returns data.attributes.status
+func (s *Service) Status() string {
+	return s.Attributes.Status
+}
+
+// SubDomain returns data.attributes.subdomain
+func (s *Service) SubDomain() string {
+	return s.Attributes.SubDomain
+}
+
+// EndPoint returns data.attributes.endpoint
+func (s *Service) EndPoint() string {
+	return s.Attributes.EndPoint
+}
+
+// PlanID returns data.relationship.service_plan.data.id
+func (s *Service) PlanID() string {
+	return s.Relationships.ServicePlan.Data.ID
+}
+
+// ServiceAttr represents service.attributes object
+type ServiceAttr struct {
+	AppID                    string           `json:"app_id,omitempty"`
+	Image                    string           `json:"image"`
+	Command                  string           `json:"command"`
+	Instances                int32            `json:"instances"`
+	CPUs                     float32          `json:"cups,omitempty"`
+	Memory                   int32            `json:"memory,omitempty"`
+	Environment              []*Env           `json:"environment"`
+	Ports                    Ports            `json:"ports,omitempty"`
+	PortMappings             [][]*PortMapping `json:"port-mappings,omitempty"`
+	CreatedAt                *time.Time       `json:"created-at,omitempty"`
+	UpdatedAt                *time.Time       `json:"updated-at,omitempty"`
+	Status                   string           `json:"status,omitempty"`
+	SubDomain                string           `json:"subdomain,omitempty"`
+	EndPoint                 string           `json:"endpoint,omitempty"`
+	CustomDomains            []*CustomDomain  `json:"custom_domains,omitempty"`
+	LastInstanceFailedAt     *time.Time       `json:"last_instance_failed_at,omitempty"`
+	LastInstanceFailedStatus string           `json:"last_instance_failed_status,omitempty"`
+}
+
+// ServiceData represents service data
+type ServiceData struct {
+	Data *Service `json:"data"`
+}
+
+// ServiceID returns data.id
+func (s *ServiceData) ServiceID() string {
+	return s.Data.ID
+}
+
+// AppID returns data.attributes.app_id
+func (s *ServiceData) AppID() string {
+	return s.Data.Attributes.AppID
+}
+
+// Type returns data.type
+func (s *ServiceData) Type() string {
+	return s.Data.Type
+}
+
+// Image returns data.attributes.image
+func (s *ServiceData) Image() string {
+	return s.Data.Image()
+}
+
+// Command returns data.attributes.command
+func (s *ServiceData) Command() string {
+	return s.Data.Command()
+}
+
+// Instances returns data.attributes.instances
+func (s *ServiceData) Instances() int32 {
+	return s.Data.Instances()
+}
+
+// CPUs returns data.attributes.cups
+func (s *ServiceData) CPUs() float32 {
+	return s.Data.CPUs()
+}
+
+// Memory returns data.attributes.memory
+func (s *ServiceData) Memory() int32 {
+	return s.Data.Memory()
+}
+
+// Environment returns data.attributes.environment
+func (s *ServiceData) Environment() []*Env {
+	return s.Data.Environment()
+}
+
+// Ports returns data.attributes.ports
+func (s *ServiceData) Ports() []*Port {
+	return s.Data.Ports()
+}
+
+// PortMappings returns data.attributes.port_mappings
+func (s *ServiceData) PortMappings() [][]*PortMapping {
+	return s.Data.PortMappings()
+}
+
+// PortMapping returns data.attributes.port_mappings[0]
+func (s *ServiceData) PortMapping() []*PortMapping {
+	return s.Data.PortMapping()
+}
+
+// CreatedAt returns data.attributes.created_at
+func (s *ServiceData) CreatedAt() *time.Time {
+	return s.Data.CreatedAt()
+}
+
+// UpdatedAt returns data.attributes.updated_at
+func (s *ServiceData) UpdatedAt() *time.Time {
+	return s.Data.UpdatedAt()
+}
+
+// Status returns data.attributes.status
+func (s *ServiceData) Status() string {
+	return s.Data.Status()
+}
+
+// SubDomain returns data.attributes.subdomain
+func (s *ServiceData) SubDomain() string {
+	return s.Data.SubDomain()
+}
+
+// EndPoint returns data.attributes.endpoint
+func (s *ServiceData) EndPoint() string {
+	return s.Data.EndPoint()
+}
+
+// PlanID returns data.relationship.service_plan.data.id
+func (s *ServiceData) PlanID() string {
+	return s.Data.PlanID()
+}

vendor/github.com/zclconf/go-cty/cty/function/stdlib/format_fsm.go
@@ -1,0 +1,358 @@
+// line 1 "format_fsm.rl"
+// This file is generated from format_fsm.rl. DO NOT EDIT.
+
+// line 5 "format_fsm.rl"
+
+package stdlib
+
+import (
+	"bytes"
+	"fmt"
+	"unicode/utf8"
+
+	"github.com/zclconf/go-cty/cty"
+)
+
+// line 20 "format_fsm.go"
+var _formatfsm_actions []byte = []byte{
+	0, 1, 0, 1, 1, 1, 2, 1, 4,
+	1, 5, 1, 6, 1, 7, 1, 8,
+	1, 9, 1, 10, 1, 11, 1, 14,
+	1, 16, 1, 17, 1, 18, 2, 3,
+	4, 2, 12, 10, 2, 12, 16, 2,
+	12, 18, 2, 13, 14, 2, 15, 10,
+	2, 15, 18,
+}
+
+var _formatfsm_key_offsets []byte = []byte{
+	0, 0, 14, 27, 34, 36, 39, 43,
+	51,
+}
+
+var _formatfsm_trans_keys []byte = []byte{
+	32, 35, 37, 43, 45, 46, 48, 91,
+	49, 57, 65, 90, 97, 122, 32, 35,
+	43, 45, 46, 48, 91, 49, 57, 65,
+	90, 97, 122, 91, 48, 57, 65, 90,
+	97, 122, 49, 57, 93, 48, 57, 65,
+	90, 97, 122, 46, 91, 48, 57, 65,
+	90, 97, 122, 37,
+}
+
+var _formatfsm_single_lengths []byte = []byte{
+	0, 8, 7, 1, 0, 1, 0, 2,
+	1,
+}
+
+var _formatfsm_range_lengths []byte = []byte{
+	0, 3, 3, 3, 1, 1, 2, 3,
+	0,
+}
+
+var _formatfsm_index_offsets []byte = []byte{
+	0, 0, 12, 23, 28, 30, 33, 36,
+	42,
+}
+
+var _formatfsm_indicies []byte = []byte{
+	1, 2, 3, 4, 5, 6, 7, 10,
+	8, 9, 9, 0, 1, 2, 4, 5,
+	6, 7, 10, 8, 9, 9, 0, 13,
+	11, 12, 12, 0, 14, 0, 15, 14,
+	0, 9, 9, 0, 16, 19, 17, 18,
+	18, 0, 20, 3,
+}
+
+var _formatfsm_trans_targs []byte = []byte{
+	0, 2, 2, 8, 2, 2, 3, 2,
+	7, 8, 4, 3, 8, 4, 5, 6,
+	3, 7, 8, 4, 1,
+}
+
+var _formatfsm_trans_actions []byte = []byte{
+	7, 17, 9, 3, 15, 13, 25, 11,
+	43, 29, 19, 27, 49, 46, 21, 0,
+	37, 23, 40, 34, 1,
+}
+
+var _formatfsm_eof_actions []byte = []byte{
+	0, 31, 31, 31, 31, 31, 31, 31,
+	5,
+}
+
+const formatfsm_start int = 8
+const formatfsm_first_final int = 8
+const formatfsm_error int = 0
+
+const formatfsm_en_main int = 8
+
+// line 19 "format_fsm.rl"
+
+func formatFSM(format string, a []cty.Value) (string, error) {
+	var buf bytes.Buffer
+	data := format
+	nextArg := 1 // arg numbers are 1-based
+	var verb formatVerb
+
+	// line 153 "format_fsm.rl"
+
+	// Ragel state
+	p := 0          // "Pointer" into data
+	pe := len(data) // End-of-data "pointer"
+	cs := 0         // current state (will be initialized by ragel-generated code)
+	ts := 0
+	te := 0
+	eof := pe
+
+	// Keep Go compiler happy even if generated code doesn't use these
+	_ = ts
+	_ = te
+	_ = eof
+
+	// line 121 "format_fsm.go"
+	{
+		cs = formatfsm_start
+	}
+
+	// line 126 "format_fsm.go"
+	{
+		var _klen int
+		var _trans int
+		var _acts int
+		var _nacts uint
+		var _keys int
+		if p == pe {
+			goto _test_eof
+		}
+		if cs == 0 {
+			goto _out
+		}
+	_resume:
+		_keys = int(_formatfsm_key_offsets[cs])
+		_trans = int(_formatfsm_index_offsets[cs])
+
+		_klen = int(_formatfsm_single_lengths[cs])
+		if _klen > 0 {
+			_lower := int(_keys)
+			var _mid int
+			_upper := int(_keys + _klen - 1)
+			for {
+				if _upper < _lower {
+					break
+				}
+
+				_mid = _lower + ((_upper - _lower) >> 1)
+				switch {
+				case data[p] < _formatfsm_trans_keys[_mid]:
+					_upper = _mid - 1
+				case data[p] > _formatfsm_trans_keys[_mid]:
+					_lower = _mid + 1
+				default:
+					_trans += int(_mid - int(_keys))
+					goto _match
+				}
+			}
+			_keys += _klen
+			_trans += _klen
+		}
+
+		_klen = int(_formatfsm_range_lengths[cs])
+		if _klen > 0 {
+			_lower := int(_keys)
+			var _mid int
+			_upper := int(_keys + (_klen << 1) - 2)
+			for {
+				if _upper < _lower {
+					break
+				}
+
+				_mid = _lower + (((_upper - _lower) >> 1) & ^1)
+				switch {
+				case data[p] < _formatfsm_trans_keys[_mid]:
+					_upper = _mid - 2
+				case data[p] > _formatfsm_trans_keys[_mid+1]:
+					_lower = _mid + 2
+				default:
+					_trans += int((_mid - int(_keys)) >> 1)
+					goto _match
+				}
+			}
+			_trans += _klen
+		}
+
+	_match:
+		_trans = int(_formatfsm_indicies[_trans])
+		cs = int(_formatfsm_trans_targs[_trans])
+
+		if _formatfsm_trans_actions[_trans] == 0 {
+			goto _again
+		}
+
+		_acts = int(_formatfsm_trans_actions[_trans])
+		_nacts = uint(_formatfsm_actions[_acts])
+		_acts++
+		for ; _nacts > 0; _nacts-- {
+			_acts++
+			switch _formatfsm_actions[_acts-1] {
+			case 0:
+				// line 29 "format_fsm.rl"
+
+				verb = formatVerb{
+					ArgNum: nextArg,
+					Prec:   -1,
+					Width:  -1,
+				}
+				ts = p
+
+			case 1:
+				// line 38 "format_fsm.rl"
+
+				buf.WriteByte(data[p])
+
+			case 4:
+				// line 49 "format_fsm.rl"
+
+				// We'll try to slurp a whole UTF-8 sequence here, to give the user
+				// better feedback.
+				r, _ := utf8.DecodeRuneInString(data[p:])
+				return buf.String(), fmt.Errorf("unrecognized format character %q at offset %d", r, p)
+
+			case 5:
+				// line 56 "format_fsm.rl"
+
+				verb.Sharp = true
+
+			case 6:
+				// line 59 "format_fsm.rl"
+
+				verb.Zero = true
+
+			case 7:
+				// line 62 "format_fsm.rl"
+
+				verb.Minus = true
+
+			case 8:
+				// line 65 "format_fsm.rl"
+
+				verb.Plus = true
+
+			case 9:
+				// line 68 "format_fsm.rl"
+
+				verb.Space = true
+
+			case 10:
+				// line 72 "format_fsm.rl"
+
+				verb.ArgNum = 0
+
+			case 11:
+				// line 75 "format_fsm.rl"
+
+				verb.ArgNum = (10 * verb.ArgNum) + (int(data[p]) - '0')
+
+			case 12:
+				// line 79 "format_fsm.rl"
+
+				verb.HasWidth = true
+
+			case 13:
+				// line 82 "format_fsm.rl"
+
+				verb.Width = 0
+
+			case 14:
+				// line 85 "format_fsm.rl"
+
+				verb.Width = (10 * verb.Width) + (int(data[p]) - '0')
+
+			case 15:
+				// line 89 "format_fsm.rl"
+
+				verb.HasPrec = true
+
+			case 16:
+				// line 92 "format_fsm.rl"
+
+				verb.Prec = 0
+
+			case 17:
+				// line 95 "format_fsm.rl"
+
+				verb.Prec = (10 * verb.Prec) + (int(data[p]) - '0')
+
+			case 18:
+				// line 99 "format_fsm.rl"
+
+				verb.Mode = rune(data[p])
+				te = p + 1
+				verb.Raw = data[ts:te]
+				verb.Offset = ts
+
+				err := formatAppend(&verb, &buf, a)
+				if err != nil {
+					return buf.String(), err
+				}
+				nextArg = verb.ArgNum + 1
+
+				// line 324 "format_fsm.go"
+			}
+		}
+
+	_again:
+		if cs == 0 {
+			goto _out
+		}
+		p++
+		if p != pe {
+			goto _resume
+		}
+	_test_eof:
+		{
+		}
+		if p == eof {
+			__acts := _formatfsm_eof_actions[cs]
+			__nacts := uint(_formatfsm_actions[__acts])
+			__acts++
+			for ; __nacts > 0; __nacts-- {
+				__acts++
+				switch _formatfsm_actions[__acts-1] {
+				case 2:
+					// line 42 "format_fsm.rl"
+
+				case 3:
+					// line 45 "format_fsm.rl"
+
+					return buf.String(), fmt.Errorf("invalid format string starting at offset %d", p)
+
+				case 4:
+					// line 49 "format_fsm.rl"
+
+					// We'll try to slurp a whole UTF-8 sequence here, to give the user
+					// better feedback.
+					r, _ := utf8.DecodeRuneInString(data[p:])
+					return buf.String(), fmt.Errorf("unrecognized format character %q at offset %d", r, p)
+
+					// line 363 "format_fsm.go"
+				}
+			}
+		}
+
+	_out:
+		{
+		}
+	}
+
+	// line 171 "format_fsm.rl"
+
+	// If we fall out here without being in a final state then we've
+	// encountered something that the scanner can't match, which should
+	// be impossible (the scanner matches all bytes _somehow_) but we'll
+	// flag it anyway rather than just losing data from the end.
+	if cs < formatfsm_first_final {
+		return buf.String(), fmt.Errorf("extraneous characters beginning at offset %d", p)
+	}
+
+	return buf.String(), nil
+}